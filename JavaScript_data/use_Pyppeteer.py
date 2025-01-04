bda5f912f09a402c436467de21d4f882d2a47030
Merthon <imchenxing42@gmail>
2025-01-04 10:39:15 +0800

    used Pyppeteer

---
+++ JavaScript_data/use_Pyppeteer.py
@@ -0,0 +1,16 @@
+# pyppeteer的使用
+import asyncio
+from pyppeteer import launch
+from pyquery import PyQuery as pq
+
+async def main():
+    browser = await launch()
+    page = await browser.newPage()
+    await page.goto('https://spa2.scrape.center/')
+    await page.waitForSelector('.item .name')
+    doc = pq(await page.content())
+    names = [item.text() for item in doc('.item .name').items]
+    print('names:', names)
+    await browser.close()
+
+asyncio.get_event_loop().run_until_complete(main())
